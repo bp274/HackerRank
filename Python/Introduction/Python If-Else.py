5b3cdd83cab9a817ab39873bc3cdc08adf0821ed
Bhaavik Pratyush <bhaavikpratyush@gmail.com>
2018-01-29 01:09:24 +0530

    Update Python If-Else.py

--- Python/Introduction/Python If-Else.py
+++ Python/Introduction/Python If-Else.py
@@ -1,11 +1,10 @@
-if __name__ == '__main__':
-    n = int(raw_input())
-    if n%2 == 1 :
+n = int(raw_input())
+if n%2 == 1 :
+    print("Weird")
+else:
+    if n>=2 and n<=5 :
+        print("Not Weird")
+    elif n<=20 :
         print("Weird")
     else:
-        if n>=2 and n<=5 :
-            print("Not Weird")
-        elif n>=6 and n<=20 :
-            print("Weird")
-        else:
-            print("Not Weird")
+        print("Not Weird")
